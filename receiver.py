27caa44f70c09e1d5e8d64c5c62e2cf64199702c
klaas <klaas-richel@hotmail.com>
2022-06-03 02:20:24 +0200

    FFT tests

--- receiver.py
+++ receiver.py
@@ -7,48 +7,115 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main():
-    symbol_length_samples = 50
-    cycles_per_symbol = 0.5
+def fft_symbols(signal):
+    # split signal into sections of symbols
+    signal_sections = np.split(signal, len(signal) // symbol_length_samples)
+    # perform transformations
+    transformations = fft(signal_sections, norm='ortho')
 
+    # take positive frequencies
+    transformations = transformations[:, 1:len(transformations[0]) // 2]
+
+    # find the loudest frequency indices
+    loud_indices = np.argmax(np.abs(transformations), axis=1)
+
+    # select those values from transformations
+    values = np.take_along_axis(transformations, loud_indices.reshape(len(transformations), 1), axis=1).flatten()
+
+    return np.mod(np.round((np.arctan2(values.imag, values.real) + np.pi) / np.pi * 2 - 2), 4).astype('byte')
+
+
+overflowcount = 0
+
+
+def stream_read_left_float32(stream, amount):
+    global overflowcount
+
+    # read samples
+    window, overflow = stream.read(amount)
+    if overflow:
+        overflowcount += 1
+        print(f'overflow number {overflowcount}')
+    if overflowcount > 10:
+        print("OVERFLOWED ALL IS LOST")
+        exit(1)
+
+    # keep only left channel and convert to float32
+    return (window[:, 0] / 2147483647).astype('float32')
+
+
+def determine_snr(stream):
+    # record two seconds for noise
+    noise_measurement = stream_read_left_float32(stream, 48000 * 2)
+    # return loudest value
+    return np.amax(np.abs(noise_measurement))
+
+
+def main():
     name = 'Loopback: PCM (hw:2,1)'
-    test = sd.query_devices(device=name, kind='input')
 
     stream = sd.InputStream(device=name, samplerate=48000, channels=2, dtype='int32')
     stream.start()
 
-    data, overflow = stream.read(1024)
+    print('discovering noise floor')
+    noise_floor = determine_snr(stream)
+    print(f'noise floor at {noise_floor}')
 
-    while np.amax(data) == 0:
-        data, overflow = stream.read(symbol_length_samples)
+    # twenty seconds of buffer
+    data = np.empty(48000 * 20, dtype='float32')
+    record_index = 0
 
+    # record half seconds at a time waiting for broken silence
+    silence_detector_chunk_size = 48000 // 2
+    candidate = np.zeros(silence_detector_chunk_size)
 
+    print("waiting for sound")
 
+    # while no samples exceed noise floor by 10%
+    while not np.any(np.abs(candidate) > (noise_floor * snr_factor)):
+        candidate = stream_read_left_float32(stream, silence_detector_chunk_size)
+
+    print("sound detected")
+
+    # record while sound exceeds noise floor
+    while np.any(np.abs(candidate) > (noise_floor * snr_factor)):
+        if record_index * silence_detector_chunk_size >= len(data):
+            print("sound too long!")
+            break
+
+        dst_index = record_index * silence_detector_chunk_size
+        data[dst_index:dst_index + silence_detector_chunk_size] = candidate
+        record_index += 1
+        candidate = stream_read_left_float32(stream, silence_detector_chunk_size)
+
+    print("sound stopped!")
+    signal_sections = np.split(data, len(data) // symbol_length_samples // 10)
+    for i in signal_sections:
+        plt.plot(i)
+        plt.show()
+
+    # slide across capture until preamble
+    preamble_found = False
+    while not preamble_found:
+        # slide across until halfway, end of transform reached end of window
+        for i in range(len(data)):
+            angles = fft_symbols(data[i:])
+            if preamble in angles:
+                print("We found the preamble!")
 
     stream.stop()
     stream.close()
 
-    # plt.rcParams['figure.dpi'] = 300
-    #
-    # signal_sections = np.split(signal, len(signal) // symbol_length_samples)
-    # transformations = fft(signal_sections, norm='ortho')
-    #
-    # # take positive frequencies
-    # transformations = transformations[:, 1:len(transformations[0]) // 2]
-    #
-    # # find the loudest frequency indices
-    # loud_indices = np.argmax(np.abs(transformations), axis=1)
-    #
-    # # select those values from transformations
-    # values = np.take_along_axis(transformations,loud_indices.reshape(len(transformations),1) ,axis=1).flatten()
-    #
-    # # plot the stuff
-    # plt.scatter(values.real, values.imag, 10, cmap=plt.cm.rainbow, c=np.linspace(0, 1, len(values)))
-    # plt.xlim(-1, 1)
-    # plt.ylim(-1, 1)
-    # plt.show()
-    # plt.cla()
-
 
 if __name__ == '__main__':
+    symbol_length_samples = 250
+    cycles_per_symbol = 1
+
+    preamble = [0, 2, 1, 3, 0, 0, 1, 1, 2, 2, 3, 3]
+    preamble_num_samples = symbol_length_samples * len(preamble)
+
+    # 10% over noise
+    snr_desired = 10
+    snr_factor = 1. + (snr_desired / 100.)
+
     main()
--- transmitter.py
+++ transmitter.py
@@ -1,12 +1,11 @@
 import sys
+from time import sleep
 
 from scipy.fft import fft
 
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
-
-test_phrase = b'this is a test of QAM. I really really hope it works out well!'
 
 
 def bytes_to_symbols(data):
@@ -18,17 +17,55 @@ def bytes_to_symbols(data):
         symbols[i * 4 + 3] = (v >> 0) & 0b11
     return symbols
 
-def main():
-    symbol_length_samples = 50
-    cycles_per_symbol = 0.5
 
+def fft_symbols(signal):
+    plt.plot(signal)
+    plt.show()
+    # split signal into sections of symbols
+    signal_sections = np.split(signal, len(signal) // symbol_length_samples)
+    # perform transformations
+    transformations = fft(signal_sections, norm='ortho')
+
+    # take positive frequencies
+    transformations = transformations[:, 1:len(transformations[0]) // 2]
+
+    # find the loudest frequency indices
+    loud_indices = np.argmax(np.abs(transformations), axis=1)
+
+    # select those values from transformations
+    values = np.take_along_axis(transformations, loud_indices.reshape(len(transformations), 1), axis=1).flatten()
+    plt.scatter(values.real, values.imag)
+    plt.xlim(-2,2)
+    plt.ylim(-2,2)
+    plt.show()
+
+    return np.mod(np.round((np.arctan2(values.imag, values.real) + np.pi) / np.pi * 2 - 2), 4).astype('byte')
+
+def angle(x,y):
+    return np.mod(np.round((np.arctan2(y,x) + np.pi) / np.pi * 2 - 2), 4)
+
+
+def main():
     sample_space = np.linspace(0, cycles_per_symbol, symbol_length_samples)
     phases = [0, 0.5, 1, 1.5]
     symbol_signals = []
     for i in phases:
         symbol_signals.append((np.cos(2 * np.pi * sample_space + i * np.pi) * 0.2 * 2147483647).astype('int32'))
 
-    symbol_signals = [np.hstack((i.reshape(len(i),1), np.zeros( (len(sample_space), 1) , dtype='int32') )) for i in symbol_signals]
+    symbol_signals = [np.hstack((i.reshape(len(i), 1), np.zeros((len(sample_space), 1), dtype='int32'))) for i in
+                      symbol_signals]
+
+    print(angle(1,0))
+    print(angle(0,1))
+    print(angle(-1,0.1))
+    print(angle(0,-1))
+    print(angle(-1,-0.1))
+    print(angle(0,-1))
+    print(fft_symbols(symbol_signals[0][:,0] / 2147483647.))
+    print(fft_symbols(symbol_signals[1][:,0] / 2147483647.))
+    print(fft_symbols(symbol_signals[2][:,0] / 2147483647.))
+    print(fft_symbols(symbol_signals[3][:,0] / 2147483647.))
+    return
 
     with open(sys.argv[1], "rb") as f:
         filedata = f.read()
@@ -40,13 +77,28 @@ def main():
     name = 'Loopback: PCM (hw:2,0)'
     test = sd.query_devices(device=name, kind='output')
     stream = sd.OutputStream(samplerate=48000, device=name, channels=2, dtype='int32')
+    # stream = sd.OutputStream(samplerate=48000, device=sd.default.device, channels=2, dtype='int32')
 
     stream.start()
-    for i in symbols:
+    # send preamble
+    for i in preamble:
         stream.write(symbol_signals[i])
+
+    sleep(1)
+
+    # send data
+    # for i in symbols:
+        # stream.write(symbol_signals[i])
     stream.stop()
     stream.close()
 
 
 if __name__ == '__main__':
+    test_phrase = b'this is a test of QAM. I really really hope it works out well!'
+
+    preamble = [0, 2, 1, 3, 0, 0, 1, 1, 2, 2, 3, 3]
+
+    symbol_length_samples = 250
+    cycles_per_symbol = 1
+
     main()
